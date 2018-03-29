17337fc1ba5e1dff0fdd433baf70a47d2c072662
Thandohimself <thandohimself@gmail.com>
2018-03-30 00:58:22 +0200

    Welcome Screen working
    Welcome Screen working

--- app/src/main/java/com/example/thando/qvaya/startscreen.java
+++ app/src/main/java/com/example/thando/qvaya/startscreen.java
@@ -3,22 +3,46 @@ package com.example.thando.qvaya;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class startscreen extends AppCompatActivity {
 
      VideoView videoHolder;
+    private ViewPager viewPager;
+    private WelcomeActivity.MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+    private PreferenceManager prefManager;
 
+    private void launchHomeScreen() {
+        prefManager.setFirstTimeLaunch(false);
+        startActivity(new Intent(startscreen.this, HomeActivity.class));
+        finish();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
+
+        // Checking for first time launch - before calling setContentView()
+        prefManager = new PreferenceManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -53,7 +77,7 @@ public class startscreen extends AppCompatActivity {
     private void jump() {
         if(isFinishing())
             return;
-        startActivity(new Intent(this, WelcomeScreen.class));
+        startActivity(new Intent(this, WelcomeActivity.class));
         finish();
     }
 }
