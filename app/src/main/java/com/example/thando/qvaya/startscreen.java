7a54101cc4ef7f9360e8fb4431cfc42de5803185
Thandohimself <thandohimself@gmail.com>
2018-03-29 22:03:49 +0200

    WORKING --- StartScreen only

---
+++ app/src/main/java/com/example/thando/qvaya/startscreen.java
@@ -0,0 +1,59 @@
+package com.example.thando.qvaya;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class startscreen extends AppCompatActivity {
+
+     VideoView videoHolder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        setContentView(R.layout.activity_startscreen);
+        videoHolder = (VideoView) findViewById(R.id.vid1);
+        try{
+
+            Uri video = Uri.parse("android.resource://" + getPackageName() + "/"
+                    + R.raw.s8);
+            videoHolder.setVideoURI(video);
+
+            videoHolder.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                public void onCompletion(MediaPlayer mp) {
+                    jump();
+                }
+
+            });
+            videoHolder.start();
+        } catch(Exception ex) {
+            jump();
+        }
+
+
+
+    }
+
+    private void jump() {
+        if(isFinishing())
+            return;
+        startActivity(new Intent(this, WelcomeScreen.class));
+        finish();
+    }
+}
